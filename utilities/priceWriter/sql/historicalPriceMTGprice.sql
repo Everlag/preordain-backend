9e8b751d19561ae4b9e558f951ed853a87ff2b46
Everlag <kyleveb@gmail.com>
2015-06-28 13:22:16 -0600

    Removed sql that was manually copied to priceWriter before the switch to embedding sql inside priceDB

--- utilities/priceWriter/sql/historicalPriceMTGprice.sql
+++
@@ -1,6 +0,0 @@
-/*
-Returns all prices for a printing of a specific card
-*/
-
-SELECT * FROM prices.mtgprice WHERE
-name=$1 AND set=$2
--- utilities/priceWriter/sql/medianMKM.sql
+++
@@ -1,7 +0,0 @@
-/*
-Returns the weekly aggregate median for a name and set combination
-*/
-select date_trunc('week', time), median(price) from prices.magiccardmarket
-where
-name=$1 and set=$2
-group by date_trunc('week', time) order by date_trunc('week', time) desc;
