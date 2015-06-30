b2e90d4730ed393dad371903a7a165b21f9f8666
Everlag <kyleveb@gmail.com>
2015-06-30 00:28:25 -0600

    Cleared out sql made unnecessary by moving it into the userDB package itself.

--- api/Users/sql/removeSession.sql
+++
@@ -1,9 +0,0 @@
-/*
-Removes a provided session for a user
-
-Takes:
-	name - string, user that owns it
-	sessionKey - []byte, a valid session key
-*/
-
-DELETE FROM users.sessions WHERE name=$1 AND sessionKey=$2
