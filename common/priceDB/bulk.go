355ab1c6d7c5b3925bbcd46163ba49412134823c
Everlag <kyleveb@gmail.com>
2016-01-03 16:39:28 -0700

    smarter bulk price fetches
    Previously, any single card for a batch would compromise the entire set
    of prices. This allows up to 1/3 of a bulk fetch to fail.
    
    closes #11

--- common/priceDB/bulk.go
+++ common/priceDB/bulk.go
@@ -2,6 +2,8 @@ package priceDB
 
 import (
 	"github.com/jackc/pgx"
+
+	"fmt"
 )
 
 func GetBulkLatestLowest(pool *pgx.ConnPool,
@@ -9,13 +11,30 @@ func GetBulkLatestLowest(pool *pgx.ConnPool,
 
 	result:= make(Prices, len(names))
 
+	failed:= 0
+
 	for i, c:= range names {
 		p, err:= GetCardLatestLowest(pool, c, source)
 		if err!=nil {
-			return nil, err
+			// Record this as a failure
+			failed++
+
+			// Insert a placeholder
+			p = Price{
+				Name: c,
+				Set: "Unknown",
+				Price: -1,
+				Source: source,
+			}
 		}
 
 		result[i] = p
+	}
+
+	// If we fail to fetch more than 1/3 of prices for this bulk
+	// set, we error out
+	if failed >= len(names) / 3 {
+		return nil, fmt.Errorf("too many fetch failures")
 	}
 
 	return result, nil
@@ -26,13 +45,30 @@ func GetBulkLatestHighest(pool *pgx.ConnPool,
 
 	result:= make(Prices, len(names))
 
+	failed:= 0
+
 	for i, c:= range names {
 		p, err:= GetCardLatestHighest(pool, c, source)
 		if err!=nil {
-			return nil, err
+			// Record this as a failure
+			failed++
+
+			// Insert a placeholder
+			p = Price{
+				Name: c,
+				Set: "Unknown",
+				Price: -1,
+				Source: source,
+			}
 		}
 
 		result[i] = p
+	}
+
+	// If we fail to fetch more than 1/3 of prices for this bulk
+	// set, we error out
+	if failed >= len(names) / 3 {
+		return nil, fmt.Errorf("too many fetch failures")
 	}
 
 	return result, nil
